6f126eda7052f6ef42647abd4e460d5cc15348f2
Dominik Obermaier <dominik.obermaier@dc-square.de>
2014-03-02 17:09:21 +0100

    improved javadoc of the subscription store

--- src/main/java/com/dcsquare/hivemq/spi/services/SubscriptionStore.java
+++ src/main/java/com/dcsquare/hivemq/spi/services/SubscriptionStore.java
@@ -14,33 +14,46 @@ import java.util.Set;
 public interface SubscriptionStore {
 
     /**
-     * @return all subscriptions as a pair of client identifier and subscribed topic
+     * This method returns all subscriptions on HiveMQ as a {@link Multimap} of client identifiers and topics.
+     * <p/>
+     * Please be aware that calling this method very often on HiveMQ instances with many subscriptions could have
+     * negative performance effects
+     *
+     * @return a {@link Multimap} of client identifiers and their topic subscriptions
      */
     public Multimap<String, Topic> getSubscriptions();
 
     /**
-     * @param topic the subscribed topic
-     * @return client identifier of all subscribers, that subscribed on the topic
+     * Returns all MQTT client subscriber identifiers for a given topic. MQTT Wildcards are allowed.
+     *
+     * @param topic the topic
+     * @return client identifiers of all subscribers that subscribed to the topic
      */
     public Set<String> getSubscribers(String topic);
 
     /**
+     * Returns all topics a client subscribed to.
+     *
      * @param clientID of the client
-     * @return all topics the client is subscribed to
+     * @return all topics the client subscribed to
      */
     public Set<Topic> getTopics(String clientID);
 
     /**
-     * This method adds a subscription for a certain client to a certain topic
+     * This method adds a subscription for a certain client to a certain topic.
+     * <p/>
+     * If the clientId or the topic are <code>null</code>, nothing will happen.
+     *
      * @param clientID client, which should be subscribed
-     * @param topic topic to which the client should be subscribed
+     * @param topic    topic to which the client should be subscribed
      */
     public void addSubscription(String clientID, Topic topic);
 
     /**
      * This method removes a subscription for a certain client to a certain topic
+     *
      * @param clientID client, which should be unsubscribed
-     * @param topic topic from which the client should be unsubscribed
+     * @param topic    topic from which the client should be unsubscribed
      */
     public void removeSubscription(String clientID, String topic);
 
