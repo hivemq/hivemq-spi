fa9fc295687cdc96c3861ba21e72c290d190c956
Dominik Obermaier <dominik.obermaier@dc-square.de>
2014-03-02 16:03:16 +0100

    api doc improvements

--- src/main/java/com/dcsquare/hivemq/spi/services/RetainedMessageStore.java
+++ src/main/java/com/dcsquare/hivemq/spi/services/RetainedMessageStore.java
@@ -14,45 +14,51 @@ import java.util.Set;
 public interface RetainedMessageStore {
 
     /**
-     * @return all retained messages, which are currently stored
+     * @return all retained messages which are currently stored
      */
     public Set<RetainedMessage> getRetainedMessages();
 
     /**
      * @param topic a topic
-     * @return retained message for the specific topic, otherwise Optional.absent
+     * @return retained message for the specific topic, otherwise an {@link com.google.common.base.Optional}
+     *         instance with an empty reference
      */
     public Optional<RetainedMessage> getRetainedMessage(String topic);
 
     /**
-     * This removes the retained message of the given topic
+     * Removes the retained message from given topic. <code>null</code> values are ignored.
+     * If there isn't any retained message on the topic yet, nothing will happen.
      *
      * @param topic from which the message should be removed
      */
     public void remove(String topic);
 
     /**
-     * This removes the retained message
+     * Removes a given retained message. <code>null</code> values are ignored.
+     * If the given retained message doesn't exist, nothing will happen.
      *
      * @param retainedMessage which should be removed
      */
     public void remove(RetainedMessage retainedMessage);
 
     /**
-     * Removes all retained messages
+     * Removes all retained messages from the message store.
      */
     public void clear();
 
     /**
-     * This method adds or replaces a retained message to a topic with a message and QoS
-     * @param retainedMessage which should be added
+     * This method adds or replaces a retained message
+     *
+     * @param retainedMessage which should be added or replaced
      */
     public void addOrReplace(RetainedMessage retainedMessage);
 
     /**
-     * Checks if the retained message is present in the retained message store
-     * @param retainedMessage which should be checked
-     * @return true if the message is present, otherwise false
+     * Checks if the retained message is present in the retained message store.
+     * Only the topic is of a retained message is considered for this check, QoS and message are ignored.
+     *
+     * @param retainedMessage to check if it's already in the message store
+     * @return true if there's already a message on the topic of the given retained message
      */
     public boolean contains(RetainedMessage retainedMessage);
 
