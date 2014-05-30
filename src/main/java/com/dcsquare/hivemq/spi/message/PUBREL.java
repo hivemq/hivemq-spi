786656f847c6db76f443c02e9fb7581d75ac75c0
Dominik Obermaier <dominik.obermaier@dc-square.de>
2014-05-30 16:42:07 +0200

    updated copyright

--- src/main/java/com/dcsquare/hivemq/spi/message/PUBREL.java
+++ src/main/java/com/dcsquare/hivemq/spi/message/PUBREL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 dc-square GmbH
+ * Copyright 2014 dc-square GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
