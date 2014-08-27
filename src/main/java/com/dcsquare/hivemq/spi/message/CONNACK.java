1d835256db1454df1a35817a5055f67e581fcb78
Dominik Obermaier <dominik.obermaier@dc-square.de>
2014-08-27 15:41:25 +0200

    added mqtt 3.1.1 sessionPresent flag to CONNACK message

--- src/main/java/com/dcsquare/hivemq/spi/message/CONNACK.java
+++ src/main/java/com/dcsquare/hivemq/spi/message/CONNACK.java
@@ -25,13 +25,44 @@ public class CONNACK extends Message {
 
     private final ReturnCode returnCode;
 
+    /**
+     * Session Present is a MQTT 3.1.1 Feature and
+     * is ignored in MQTT 3.1 environments.
+     *
+     * @see <a href="http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/mqtt-v3.1.1.html#_Toc385349255">
+     *      MQTT Session Present Flag</a>
+     */
+    private boolean sessionPresent;
+
     public CONNACK(final ReturnCode returnCode) {
+        this(returnCode, false);
+    }
+
+    public CONNACK(final ReturnCode returnCode, final boolean sessionPresent) {
+
+        if (returnCode != ReturnCode.ACCEPTED && sessionPresent) {
+            throw new IllegalArgumentException("The sessionPresent flag is only allowed for return code " + ReturnCode.ACCEPTED);
+        }
         this.returnCode = returnCode;
+        this.sessionPresent = sessionPresent;
     }
 
     public ReturnCode getReturnCode() {
         return returnCode;
     }
+
+    /**
+     * Returns <code>true</code> if there is already a session present on the
+     * MQTT Broker for a client. Returns <false></false> if the client
+     * has a clean session
+     *
+     * @return if there is a session present on the MQTT broker
+     * @since 2.1.0
+     */
+    public boolean isSessionPresent() {
+        return sessionPresent;
+    }
+
 
     @Override
     public boolean equals(final Object o) {
@@ -40,6 +71,7 @@ public class CONNACK extends Message {
 
         final CONNACK connack = (CONNACK) o;
 
+        if (sessionPresent != connack.sessionPresent) return false;
         if (returnCode != connack.returnCode) return false;
 
         return true;
@@ -47,6 +79,8 @@ public class CONNACK extends Message {
 
     @Override
     public int hashCode() {
-        return returnCode != null ? returnCode.hashCode() : 0;
+        int result = returnCode.hashCode();
+        result = 31 * result + (sessionPresent ? 1 : 0);
+        return result;
     }
 }
---
+++ src/test/java/com/dcsquare/hivemq/spi/message/CONNACKTest.java
@@ -0,0 +1,24 @@
+package com.dcsquare.hivemq.spi.message;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Dominik Obermaier
+ */
+public class CONNACKTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_throw_illegal_argument_if_session_present_on_non_accepted_return_code() throws Exception {
+        new CONNACK(ReturnCode.REFUSED_NOT_AUTHORIZED, true);
+    }
+
+    @Test
+    public void test_session_present_non_accepted_return_code() throws Exception {
+        final CONNACK connack = new CONNACK(ReturnCode.ACCEPTED, true);
+
+        assertEquals(true, connack.isSessionPresent());
+        assertEquals(ReturnCode.ACCEPTED, connack.getReturnCode());
+    }
+}
