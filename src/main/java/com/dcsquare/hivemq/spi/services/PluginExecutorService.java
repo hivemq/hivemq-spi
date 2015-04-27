e500f2f1a2a7d7b409b1142489daab3bbe7f061c
Christoph Schäbel <christoph.schaebel@dc-square.de>
2015-04-27 11:25:55 +0200

    add doc for shutdown methods in PluginExecutorService

--- src/main/java/com/dcsquare/hivemq/spi/services/PluginExecutorService.java
+++ src/main/java/com/dcsquare/hivemq/spi/services/PluginExecutorService.java
@@ -2,6 +2,8 @@ package com.dcsquare.hivemq.spi.services;
 
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 
+import java.util.List;
+
 /**
  * A shared thread pool executor which is a {@link ListeningScheduledExecutorService}.
  * It is recommended to use this instead of creating your own thread(-pool) in your plugin.
@@ -9,5 +11,25 @@ import com.google.common.util.concurrent.ListeningScheduledExecutorService;
  * @author Christoph Schäbel
  */
 public interface PluginExecutorService extends ListeningScheduledExecutorService {
+
+    /**
+     * The Plugin Executor Service is automatically shut down when HiveMQ is shut down.
+     *
+     * Manual calls to this method from the plugin system are not supported.
+     *
+     * @throws {@link UnsupportedOperationException}
+     */
+    @Override
+    void shutdown();
+
+    /**
+     * The Plugin Executor Service is automatically shut down when HiveMQ is shut down.
+     *
+     * Manual calls to this method from the plugin system are not supported.
+     *
+     * @throws {@link UnsupportedOperationException}
+     */
+    @Override
+    List<Runnable> shutdownNow();
 
 }
