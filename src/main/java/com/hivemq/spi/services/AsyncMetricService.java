ae790d6030ac55368a958e7d49e3dc265abff2c8
Lukas Brandl <lukas.brandl@dc-square.de>
2016-02-12 09:27:03 +0100

    added copyright

--- src/main/java/com/hivemq/spi/services/AsyncMetricService.java
+++ src/main/java/com/hivemq/spi/services/AsyncMetricService.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2014 dc-square GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hivemq.spi.services;
 
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.hivemq.spi.annotations.Nullable;
 import com.hivemq.spi.metrics.HiveMQMetric;
 
 import java.util.Map;
@@ -23,7 +38,7 @@ public interface AsyncMetricService {
      *
      * @param metric the metric
      * @param <T>    the metric type
-     * @return the metric (if available) or <code>null</code>
+     * @return a {@link ListenableFuture} with the metric (if available) or a <code>null</code> result.
      */
     <T extends Metric> ListenableFuture<T> getHiveMQMetric(HiveMQMetric<T> metric);
 
@@ -35,7 +50,7 @@ public interface AsyncMetricService {
      *
      * @param metric the metric
      * @param <T>    the metric type
-     * @return the metric (if available) or <code>null</code>
+     * @return a {@link ListenableFuture} with the metric (if available) or a <code>null</code> result.
      */
     <T extends Metric> ListenableFuture<Map<String, T>> getClusterMetric(HiveMQMetric<T> metric);
 
